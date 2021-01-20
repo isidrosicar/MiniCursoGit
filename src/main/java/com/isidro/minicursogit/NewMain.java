ba7821d5b51605f6364b437774c3b03b57574e7e
Isidro Serrano Pineda <isidro@sicar.mx>
2021-01-20 17:45:51 -0600

    Salidas

--- src/main/java/com/isidro/minicursogit/NewMain.java
+++ src/main/java/com/isidro/minicursogit/NewMain.java
@@ -16,9 +16,9 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Mundo");
+        System.out.println("Hola MundoDisco");
         
-        System.out.println("Hola, mundo");
+        System.out.println("Hola, tierra plana");
     }
     
 }
